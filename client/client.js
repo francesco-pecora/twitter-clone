c2e3b7c4018902ab056509228bb7a28dc2355711
francesco-pecora <francescogp9898@hotmail.com>
2020-03-07 00:19:55 -0500

    request tweets from db

--- client/client.js
+++ client/client.js
@@ -2,9 +2,12 @@ console.log("It's working...");
 
 const form = document.querySelector("form");
 const loadingElement = document.querySelector(".loading");
+const tweetsElement = document.querySelector(".tweets");
 const API_URL = "http://localhost:5000/tweets";
 
-loadingElement.style.display = "none";
+loadingElement.style.display = "";
+
+listAllTweets();
 
 form.addEventListener('submit', (event) => {
     event.preventDefault(); // prevent for page reloading every request
@@ -31,6 +34,36 @@ form.addEventListener('submit', (event) => {
         console.log(createdTweet);
         form.reset(); // cleans the tweet (text) area
         form.style.display = "";
-        loadingElement.style.display = "none";
+        listAllTweets(); // make sure re-render tweets when submit
     });
 })
+
+function listAllTweets() {
+    tweetsElement.innerHTML = ""; // cleaning up the output before creating it
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(tweets => {
+            tweets.reverse();
+            tweets.forEach(tweet => {
+                // create elements to show in the page for each tweet
+                const div = document.createElement('div');
+
+                const header = document.createElement('h3');
+                header.textContent = tweet.name;
+                // textContent instead of innerHTML to avoid
+                // people inputting valid HTML that would render
+                const contents = document.createElement('p');
+                contents.textContent = tweet.content;
+
+                const date = document.createElement('small');
+                date.textContent = new Date(tweet.created);
+
+                div.appendChild(header);
+                div.appendChild(contents);
+                div.appendChild(date);
+
+                tweetsElement.appendChild(div);
+            });
+            loadingElement.style.display = "none";
+        });
+}
